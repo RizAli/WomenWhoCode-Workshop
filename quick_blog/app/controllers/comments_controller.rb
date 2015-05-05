bd71ce0f48eb4d9a4b0bca4aea983b15d10f5d04
Rizwan Ali <rizcube@gmail.com>
2015-05-05 20:29:39 +0100

    added ajax, remote true in the form, and updated create method for response

--- quick_blog/app/controllers/comments_controller.rb
+++ quick_blog/app/controllers/comments_controller.rb
@@ -3,7 +3,9 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create!(comment_params)
-    redirect_to @post
+    respond_to do |format|
+      format.htmll { redirect_to @post }
+      format.js
   end
 
   private
